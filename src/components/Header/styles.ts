b189faf43bb09713d5f8f44a60806c741b79a5b1
WillBorged1733 <134107458+ayhamyza@users.noreply.github.com>
2024-06-18 17:39:33 -0300

    Component: Summary.

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   background: ${props => props.theme["gray-900"]};
-  padding: 2.5rem 0 7.5rem;
+  padding: 4.0rem 0 12.0rem;
 `;
 
 export const HeaderContent = styled.div`
   width: 100%;
   max-width: 1120px;
   margin: 0 auto;
-  padding: 0 1.5rem;
+  padding: 0 2.4rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -21,7 +21,7 @@ export const NewTransactionButton = styled.button`
   background: ${props => props.theme["green-500"]};
   color: ${props => props.theme["white"]};
   font-weight: bold;
-  padding: 0 1.25rem;
+  padding: 0 2.0rem;
   border-radius: 6px;
   cursor: pointer;
   &:hover {
